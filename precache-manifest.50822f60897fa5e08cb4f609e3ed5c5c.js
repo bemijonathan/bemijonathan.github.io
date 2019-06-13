55aa95fe85d370757b5af9d9fe10bddc75e968e7
bemijonathan <34762800+bemijonathan@users.noreply.github.com>
2019-06-13 16:10:16 +0100

    Add files via upload
    added my portfolio with contact

---
+++ precache-manifest.50822f60897fa5e08cb4f609e3ed5c5c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "56116940fd424e3b4fdc34504fd44a15",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38318ab8135e88632b8c",
+    "url": "/static/css/main.7a643723.chunk.css"
+  },
+  {
+    "revision": "a3b08110366cb10fba0d",
+    "url": "/static/js/2.6218327a.chunk.js"
+  },
+  {
+    "revision": "38318ab8135e88632b8c",
+    "url": "/static/js/main.1a402d6c.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
